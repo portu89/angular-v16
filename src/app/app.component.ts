d4fbea9f33f79c79b634f32e8b00c29334954590
portu89 <elportu89@gmail.com>
2023-05-06 23:09:43 +0200

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,8 +7,20 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-v16';
+  counter = 0;
+
+  addCounter() {
+    if (this.counter !== 5) {
+      this.counter = this.counter + 1;
+    }
+  }
+
+  removeCounter() {
+    if (this.counter !== 0) {
+      this.counter = this.counter - 1;
+    }
+  }
 }
